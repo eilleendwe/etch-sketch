592b4623d49b8808dc9be7ba6a0c4925b277fe5d
eilleendwe <eilleenyeoh3@gmail.com>
2024-02-21 12:56:02 +0700

    adding button functions

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+//global variable for color
+let color = "black";
+
 //domcontentloaded = wait until the html file loaded first
 document.addEventListener('DOMContentLoaded', function(){
-    createBoard(16);
+    let popupBtn = document.getElementById('popup');
+    popupBtn.addEventListener("click", function() {
+        let size = getSize();
+        createBoard(size);
+    })
 
 })
+
+
 
 function createBoard(size){
     let board = document.querySelector('.board');
@@ -14,8 +23,54 @@ function createBoard(size){
 
     for (let i = 0; i < numDivs; i ++){
         let div = document.createElement('div');
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv)
         //kalau mau put div di dalam board
         board.insertAdjacentElement("beforeend", div);
     }
+}
+
+function getSize(){
+    let input = prompt("Size of the board");
+    let message = document.getElementById('message');
+    if (input == ""){
+        message.innerHTML = "Please give a number";
+    } else if (input < 0 || input > 100){
+        message.innerHTML = "Please give a number from 1-100";
+    } else {
+        message.innerHTML = "START!";
+        return input;
+    }
+}
+
+let black = document.getElementById('black-btn');
+let random = document.getElementById('random-btn');
+let reset = document.getElementById('reset-btn');
+
+black.addEventListener("click", function(){
+    setColor('black');
+});
+random.addEventListener("click", function() {
+    setColor('random');
+});
+reset.addEventListener("click", function(){
+    resetBoard();
+})
+
+
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    } else{
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = 'white');
 }
